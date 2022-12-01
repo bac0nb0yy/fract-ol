e0d8fd7c79b6848a1f94e637d2f3594ae031381c
bac0nb0yy <kyuma@pm.me>
2022-12-01 23:50:24 +0100

    Can close the window with mouse now

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: dtelnov <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 18:53:06 by dtelnov           #+#    #+#             */
-/*   Updated: 2022/11/28 12:00:55 by dtelnov          ###   ########.fr       */
+/*   Updated: 2022/12/01 22:45:05 by dtelnov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,24 @@
 # include "mlx_linux/mlx.h"
 # include <stdlib.h>
 # include <stdio.h>
+# include <errno.h>
 
-# ifndef XK_ESCAPE
-#  define XK_ESCAPE 0xff1b
-# endif
+# define XK_ESCAPE 0xff1b
+# define MAX_ITERATION 142
+# define OUTPUT_WIDTH 1080
+# define OUTPUT_HEIGHT 920
+# define START_X -0.75f
+# define START_Y 0.0f
+# define ZOOM 1.1f
+
+typedef struct s_data {
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 #endif
--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,105 +6,109 @@
 /*   By: dtelnov <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 18:52:13 by dtelnov           #+#    #+#             */
-/*   Updated: 2022/12/01 17:25:55 by dtelnov          ###   ########.fr       */
+/*   Updated: 2022/12/01 23:04:20 by dtelnov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-typedef struct s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-int	ft_close(int keycode, t_vars *vars)
+int	ft_close(int keycode, t_data *data)
 {
-	printf("Key pressed : 0x%.4x\n",keycode);
+	printf("Key pressed : 0x%.4x\n", keycode);
 	if (keycode == XK_ESCAPE)
 	{
-		mlx_destroy_window(vars->mlx, vars->win);
-		exit(0);
+		mlx_destroy_window(data->mlx, data->win);
+		exit(EXIT_SUCCESS);
 	}
 	return (0);
 }
 
-typedef struct s_data {
-	void	*mlx;
-	void	*win;
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+int	ft_close_mouse(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 int	main(void)
 {
-	t_data	all;
-	const unsigned int	MAX_ITERATION = 142;
-	const unsigned int	OUTPUT_WIDTH = 1080;
-	const unsigned int	OUTPUT_HEIGHT = 920;
-	const long double	START_X = -0.75;
-	const long double	START_Y = 0.0;
-	const long double	ZOOM = 3.0;
-	unsigned int		x = 0;
-	unsigned int		y = 0;
-
+	t_data				all;
+	unsigned int		x;
+	unsigned int		y;
+	float				p_i;
+	float				p_r;
+	float				new_r;
+	float				new_i;
+	float				old_r;
+	float				old_i;
+	unsigned int		i;
+	unsigned int		color;
+	
+	x = 0;
+	y = 0;
 	const unsigned int COLOR_TABLE[] = {
-    0xf7df, 0xff5a, 0x07ff, 0x7ffa, 0xf7ff, 0xf7bb, 0xff38, 0xff59, 0x001f, 0x895c, 
-    0xa145, 0xddd0, 0x5cf4, 0x7fe0, 0xd343, 0xfbea, 0x64bd, 0xffdb, 0xd8a7, 0x07ff, 
-    0x0011, 0x0451, 0xbc21, 0xad55, 0x0320, 0xbdad, 0x8811, 0x5345, 0xfc60, 0x9999, 
-    0x8800, 0xecaf, 0x8df1, 0x49f1, 0x2a69, 0x067a, 0x901a, 0xf8b2, 0x05ff, 0x6b4d, 
-    0x1c9f, 0xd48e, 0xb104, 0xffde, 0x2444, 0xf81f, 0xdefb, 0xffdf, 0xfea0, 0xdd24, 
-    0x8410, 0x0400, 0xafe5, 0xf7fe, 0xfb56, 0xcaeb, 0x4810, 0xfffe, 0xf731, 0xe73f, 
-    0xff9e, 0x7fe0, 0xffd9, 0xaedc, 0xf410, 0xe7ff, 0xffda, 0xd69a, 0x9772, 0xfdb8, 
-    0xfd0f, 0x2595, 0x867f, 0x839f, 0x7453, 0xb63b, 0xfffc, 0x07e0, 0x3666, 0xff9c, 
-    0xf81f, 0x8000, 0x6675, 0x0019, 0xbaba, 0x939b, 0x3d8e, 0x7b5d, 0x07d3, 0x4e99, 
-    0xc0b0, 0x18ce, 0xf7ff, 0xff3c, 0xff36, 0xfef5, 0x0010, 0xffbc, 0x8400, 0x6c64, 
-    0xfd20, 0xfa20, 0xdb9a, 0xef55, 0x9fd3, 0xaf7d, 0xdb92, 0xff7a, 0xfed7, 0xcc27, 
-    0xfe19, 0xdd1b, 0xb71c, 0x8010, 0xf800, 0xbc71, 0x435c, 0x8a22, 0xfc0e, 0xf52c, 
-    0x2c4a, 0xffbd, 0xa285, 0xc618, 0x867d, 0x6ad9, 0x7412, 0xffdf, 0x07ef, 0x4416, 
-    0xd5b1, 0x0410, 0xddfb, 0xfb08, 0x471a, 0xec1d, 0xd112, 0xf6f6, 0xffff, 0xf7be, 
-    0xffe0, 0x9e66, 0x0000
-	};
-
+		0xf7df, 0xff5a, 0x07ff, 0x7ffa, 0xf7ff, 0xf7bb, 0xff38, 0xff59, 0x001f,
+			0x895c, 0xa145, 0xddd0, 0x5cf4, 0x7fe0, 0xd343, 0xfbea, 0x64bd,
+			0xffdb, 0xd8a7, 0x07ff, 0x0011, 0x0451, 0xbc21, 0xad55, 0x0320,
+			0xbdad, 0x8811, 0x5345, 0xfc60, 0x9999, 0x8800, 0xecaf, 0x8df1,
+			0x49f1, 0x2a69, 0x067a, 0x901a, 0xf8b2, 0x05ff, 0x6b4d, 0x1c9f,
+			0xd48e, 0xb104, 0xffde, 0x2444, 0xf81f, 0xdefb, 0xffdf, 0xfea0,
+			0xdd24, 0x8410, 0x0400, 0xafe5, 0xf7fe, 0xfb56, 0xcaeb, 0x4810,
+			0xfffe, 0xf731, 0xe73f, 0xff9e, 0x7fe0, 0xffd9, 0xaedc, 0xf410,
+			0xe7ff, 0xffda, 0xd69a, 0x9772, 0xfdb8, 0xfd0f, 0x2595, 0x867f,
+			0x839f, 0x7453, 0xb63b, 0xfffc, 0x07e0, 0x3666, 0xff9c, 0xf81f,
+			0x8000, 0x6675, 0x0019, 0xbaba, 0x939b, 0x3d8e, 0x7b5d, 0x07d3,
+			0x4e99, 0xc0b0, 0x18ce, 0xf7ff, 0xff3c, 0xff36, 0xfef5, 0x0010,
+			0xffbc, 0x8400, 0x6c64, 0xfd20, 0xfa20, 0xdb9a, 0xef55, 0x9fd3,
+			0xaf7d, 0xdb92, 0xff7a, 0xfed7, 0xcc27, 0xfe19, 0xdd1b, 0xb71c,
+			0x8010, 0xf800, 0xbc71, 0x435c, 0x8a22, 0xfc0e, 0xf52c, 0x2c4a,
+			0xffbd, 0xa285, 0xc618, 0x867d, 0x6ad9, 0x7412, 0xffdf, 0x07ef,
+			0x4416, 0xd5b1, 0x0410, 0xddfb, 0xfb08, 0x471a, 0xec1d, 0xd112,
+			0xf6f6, 0xffff, 0xf7be, 0xffe0, 0x9e66, 0x0000};
 	all.mlx = mlx_init();
 	all.win = mlx_new_window(all.mlx, OUTPUT_WIDTH, OUTPUT_HEIGHT, "Fract-ol");
 	all.img = mlx_new_image(all.mlx, OUTPUT_WIDTH, OUTPUT_HEIGHT);
-	all.addr = mlx_get_data_addr(all.img, &all.bits_per_pixel, &all.line_length, &all.endian);
+	all.addr = mlx_get_data_addr(all.img, &all.bits_per_pixel, &all.line_length,
+			&all.endian);
 	while (y < OUTPUT_HEIGHT)
 	{
 		x = 0;
-		long double p_i = 1.5 * (y - OUTPUT_HEIGHT / 2.0) / (0.5 * ZOOM * OUTPUT_HEIGHT) + START_Y;
+		p_i = 1.5 * (y - OUTPUT_HEIGHT / 2.0) / (0.5 * ZOOM * OUTPUT_HEIGHT)
+			+ START_Y;
 		while (x < OUTPUT_WIDTH)
 		{
-			long double p_r = 1.5 * (x - OUTPUT_WIDTH / 2.0) / (0.5 * ZOOM * OUTPUT_WIDTH) + START_X;
-			long double new_r = 0, new_i = 0, old_r = 0, old_i = 0;
-			unsigned int i = 0;
+			p_r = 1.5 * (x - OUTPUT_WIDTH / 2.0) / (0.5 * ZOOM * OUTPUT_WIDTH)
+				+ START_X;
+			new_r = 0;
+			new_i = 0;
+			old_r = 0;
+			old_i = 0;
+			i = 0;
 			while ((new_r * new_r + new_i * new_i) < 4.0 && i < MAX_ITERATION)
 			{
-                old_r = new_r;
-                old_i = new_i;
-                new_r = old_r * old_r - old_i * old_i + p_r;
-                new_i = 2.0 * old_r * old_i + p_i;
-                ++i;
-            }
-			unsigned int color = COLOR_TABLE[i];
+				old_r = new_r;
+				old_i = new_i;
+				new_r = old_r * old_r - old_i * old_i + p_r;
+				new_i = 2.0 * old_r * old_i + p_i;
+				++i;
+			}
+			color = COLOR_TABLE[i];
 			my_mlx_pixel_put(&all, x, y, color);
 			++x;
 		}
 		++y;
 	}
 	mlx_put_image_to_window(all.mlx, all.win, all.img, 0, 0);
-	mlx_hook(all.win, 2, 1L<<0, ft_close, &all);
+	mlx_hook(all.win, 2, 1L << 0, ft_close, &all);
+	mlx_hook(all.win, 17, 0L, ft_close_mouse, &all);
 	mlx_loop(all.mlx);
 	return (0);
 }
